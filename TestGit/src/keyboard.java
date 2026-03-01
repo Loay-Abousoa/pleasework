ab449257378d7c1848991114874f039869cae540
MSI <MSI@DESKTOP-DGHJKL6>
2026-03-02 01:12:36 +0200

    trying

---
+++ TestGit/src/keyboard.java
@@ -0,0 +1,9 @@
+public class keyboard {
+	int keys;
+	int fn;
+	public keyboard(int keys, int fn) {
+		super();
+		this.keys = keys;
+		this.fn = fn;
+	}
+}
---
+++ TestGit/src/phone.java
@@ -0,0 +1,10 @@
+public class phone {
+	int idk;
+	String s;
+	public phone(int idk, String s) {
+		super();
+		this.idk = idk;
+		this.s = s;
+	}
+	
+}
